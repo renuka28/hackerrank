9b7a2c6f44859b1a2ba43ba105aa5535333edb13
renuka28 <mr.renukaprasad@gmail.com>
2019-11-28 23:04:46 -0600

    calculates quartiles and quartile ranges

--- statistics_in_python/quartiles.py
+++ statistics_in_python/quartiles.py
@@ -53,3 +53,47 @@ def quartiles(nums):
     third_quartile = mmm.median(nums[start:upto])
 
     return (first_quartile, second_quartile, third_quartile)
+
+
+# Interquartile Range
+
+# Objective
+# In this challenge, we practice calculating the interquartile range. We recommend you complete the Quartiles challenge before attempting this problem.
+
+# Task
+# The interquartile range of an array is the difference between its first () and third () quartiles (i.e., ).
+
+# Given an array,X , of  integers and an array,F , representing the respective frequencies of 's elements, construct a data set, , where each  occurs at frequency . Then calculate and print 's interquartile range, rounded to a scale of  decimal place (i.e.,  format).
+
+# Tip: Be careful to not use integer division when averaging the middle two elements for a data set with an even number of elements, and be sure to not include the median in your upper and lower data sets.
+
+# Input Format
+
+# The first line contains an integer, , denoting the number of elements in arrays  and .
+# The second line contains  space-separated integers describing the respective elements of array .
+# The third line contains  space-separated integers describing the respective elements of array .
+def inter_quartile_range(nums, freq):
+    """
+    given an array of 'nums' with corresponding frequencies in 'freq', this function calculates inter quartile range
+
+    Args:
+        nums (list of ints): nums whose inter quartile range has to be calculated
+        freq (list of ints): describes frequencies of each element in 'nums'. 
+        eg for a given nums [1, 2, 3], freq [2, 2, 1] will result in final array of [1, 1, 2, 2, 1]
+    Returns:
+        int: inter quartile range (Q3 - Q1)
+    """
+
+    if freq != None:
+        all_nums = []
+        for i in range(len(nums)):
+            for i in [nums[i]] * freq[i]:
+                all_nums.append(i)
+        nums = all_nums
+
+    nums = sorted(nums)
+    q = quartiles(nums)
+    if quartiles != None:
+        return q, (q[2] - q[0])
+    else:
+        return None
